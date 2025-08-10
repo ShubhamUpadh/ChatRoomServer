2002925354dcd5083a418d7ae2da9582d30000a6
Shubham Upadhyay <01shubhamupadhyay@gmail.com>
2025-08-10 13:32:16 +0530

    feat: add UserName and UserNameHandler

---
+++ src/main/java/org/chatroom/UserName.java
@@ -0,0 +1,27 @@
+package org.chatroom;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class UserName {
+    private static Set<String> usernames = ConcurrentHashMap.newKeySet();
+    private static final Logger logger = Logger.getLogger(UserName.class.getName());
+
+    public static boolean doesUserExist(String name) {
+        logger.log(Level.INFO, "Checking if user " + name + " exists or not");
+        return usernames.contains(name);
+    }
+
+    public static void addUser(String name){
+        logger.log(Level.INFO, "Adding user " + name);
+        usernames.add(name);
+    }
+
+    public static void deleteUser(String name){
+        logger.log(Level.INFO,"Removed user " + name);
+        usernames.remove(name);
+    }
+
+}
---
+++ src/main/java/org/chatroom/UserNameHandler.java
@@ -0,0 +1,74 @@
+package org.chatroom;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class UserNameHandler {
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private final Logger logger = Logger.getLogger(UserNameHandler.class.getName());
+
+    public UserNameHandler(Socket socket){
+        this.socket = socket;
+        try {
+            this.out = new PrintWriter(
+                    new OutputStreamWriter(socket.getOutputStream()), true
+            );
+            logger.log(Level.INFO,"Created output stream");
+            this.in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
+            logger.log(Level.INFO,"Created Input stream");
+        }
+        catch (IOException e){
+            logger.log(Level.WARNING, "Error creating input/output stream" + Arrays.toString(e.getStackTrace()));
+        }
+    }
+
+    public void setUserName(){
+        // ask the user to create a userName
+        while (!socket.isClosed()){
+            if (ableToSetUsername()){
+                logger.log(Level.INFO,"Username will be set");
+                break;
+            }
+        }
+    }
+
+    private boolean ableToSetUsername(){
+
+        try {
+            out.println("Set a username (< 9 characters)");
+            String name = in.readLine();
+
+            if (name == null){
+                logger.log(Level.WARNING,"Client disconnect while setting username");
+                return false;
+            }
+
+            if (name.length() > 8) {
+                out.println("Please enter a username of less than 9 characters");
+                return false;
+            }
+
+            if (UserName.doesUserExist(name)){
+                out.println("Username already exists " + name);
+                return false;
+            }
+            else {
+                out.println("Welcome to the server " + name);
+                UserName.addUser(name);
+                return true;
+            }
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Not able to read username" + Arrays.toString(e.getStackTrace()));
+            return false;
+        }
+    }
+
+}
